36bc28638c011d3b0099a93ad11b95d1477b545c
Paras248 <60874409+Paras248@users.noreply.github.com>
2023-02-08 13:08:43 +0530

    Used lseek, Append mode and truncate mode

--- UOS/prac-1/CopyPasteUsingSystemCalls.c
+++ UOS/prac-1/CopyPasteUsingSystemCalls.c
@@ -6,16 +6,19 @@
 #include <unistd.h>
 
 void main(){
-    int fp1 = open("original-file.txt", O_RDONLY);
-    int fp2 = open("copy-file.txt", O_WRONLY);
-    char str[100];
-    if(fp1 < 0){
-        printf("File doesn't exists");
-        exit(1);
-    }
-    size_t len = 0;
-    len = read(fp1, str, sizeof str);
-    write(fp2, str, len);
-    close(fp1);
-    close(fp2);
+    	int fp1 = open("original-file.txt", O_RDONLY);
+   	int fp2 = open("copy-file.txt",O_WRONLY);
+    	//int fp2 = open("copy-file.txt",O_WRONLY | O_APPEND);
+	//int fp2 = open("copy-file.txt",O_WRONLY | O_TRUNC);
+    	char str[100];
+    	if(fp1 < 0){
+        	printf("File doesn't exists");
+        	exit(1);
+    	}
+    	size_t len = 0;
+    	len = read(fp1, str, sizeof str);
+	//off_t offset = lseek(fp2, 5, SEEK_SET);
+	write(fp2, str, len);
+    	close(fp1);
+	close(fp2);
 }
